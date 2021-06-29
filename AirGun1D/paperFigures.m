8c32528391db54a463be3486010267fcdb3010a3
Fredric Lam <fredriclam@live.com>
2021-06-29 13:31:07 -0700

    Add temporary files

--- AirGun1D/paperFigures.m
+++ AirGun1D/paperFigures.m
@@ -22,6 +22,18 @@ futuresBubbleModel(2) = parfeval(pool, @airgunShuttleDeploy, 2, ...
     nx, true, ...
     struct('bubbleModel', 'quad'));
 
+%% Reversion
+futuresComparison = parfeval(pool, @airgunShuttleDeploy, 2, ...
+    nx, false);
+
+%% Reduce data size
+for j = 1:5
+    dataPortAreaRatio(j) = dataReduction(futuresPortAreaRatio(j));
+end
+for j = 1:6
+    dataPressures(j) = dataReduction(futuresPressures(j));
+end
+
 %% Design parameters
 % Design matrix
 portAreaRatioVector = 110/180 * ...
@@ -31,7 +43,7 @@ pressureVector = [400, 600, 800, 1000, 2000, 3000]; % psi
 for i = 1:length(portAreaRatioVector)
     portAreaRatio = portAreaRatioVector(i);
     
-    % Call function [solution, metadata] = airgunShuttleDeploy(nx)
+    % Call function [solution, metadata] = airgunShuttleDeploy(nx, ...)
     futuresPortAreaRatio(i) = parfeval(pool, @airgunShuttleDeploy, 2, ...
         nx, true, ...
         struct('airgunPortAreaRatio', portAreaRatio));
@@ -39,7 +51,7 @@ end
 for i = 1:length(pressureVector)
     airgunPressure = pressureVector(i);
     
-    % Call function [solution, metadata] = airgunShuttleDeploy(nx)
+    % Call function [solution, metadata] = airgunShuttleDeploy(nx, ...)
     futuresPressures(i) = parfeval(pool, @airgunShuttleDeploy, 2, ...
         nx, true, ...
         struct('airgunPressure', airgunPressure));
@@ -55,6 +67,10 @@ end
         airgunShuttlePostprocess( ...
         futuresBubbleModel(2).OutputArguments{1}, ...
         futuresBubbleModel(2).OutputArguments{2});
+[fullStateUncoupled] = ...
+        airgunShuttlePostprocess( ...
+        futuresComparison.OutputArguments{1}, ...
+        futuresComparison.OutputArguments{2});
     
 %% Acoustic pressure signals
 tSample = linspace(0, 5, 15000);
@@ -66,6 +82,10 @@ signalsSingle = sampleSignature( ...
 signalsQuad = sampleSignature( ...
         futuresBubbleModel(2).OutputArguments{1}, ...
         futuresBubbleModel(2).OutputArguments{2}, ...
+        tSample);
+signalsComparison = sampleSignature( ...
+        futuresComparison.OutputArguments{1}, ...
+        futuresComparison.OutputArguments{2}, ...
         tSample);
 for i = 1:length(portAreaRatioVector)
     signalsPortAreaRatio{i} = sampleSignature( ...
@@ -85,46 +105,170 @@ dataDAQ = HiTestData(25).entriesDAQ(4,1375:45000);
 % dataDAQ = HiTestData(25).entriesDAQ(4,1375:15000);
 timeDAQ = HiTestData(25).headerDAQ.SamplingInterval*(1:length(dataDAQ));
 
-figure(201);
+%% Figure 1A: Acoustic pressure
+figure(201); clf
 DAQGain = 8;
 DAQSens = 1e5/7.6; % Pa per V
 signalData = DAQGain*DAQSens*dataDAQ;
-plot(timeDAQ, signalData, '.m');
+signalDataPlot = signalData - signalData(1);
+
+subplot(2,1,1);
+pS = [fullStateSingle.portStates];
+plot(1e3*[fullStateSingle.t],[pS.massFlowPort], '-', 'LineWidth', 1.5, ...
+    'Color', [0.85, 0.33, 0.1]);
+hold on
+pS = [fullStateUncoupled.portStates];
+plot(1e3*[fullStateUncoupled.t],[pS.massFlowPort], '-', 'LineWidth', 1.5, ...
+    'Color', [236 176 31]/255);
+hold off
+
+xlim([0, 300])
+ylim([0, 600])
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 12.5, ...
+    'XMinorTick', 'on', ...
+    'YMinorTick', 'on', ...
+    'TickLength', [0.0100    0.0250]);
+xlabel('$t$ [ms]', 'Interpreter', 'latex')
+ylabel('$\dot{m}$ [kg/s]', 'Interpreter', 'latex')
+
+legend({'Coupled model', 'Uncoupled model'}, ...
+    'Location', 'best', 'Interpreter' ,'latex')
+
+subplot(2,1,2);
+plot(1e3*timeDAQ, signalDataPlot/1e3, '.-k', ...
+    'MarkerSize', 2);
 
 dwData = 1/(timeDAQ(end)-timeDAQ(1));
 omegaVecData = [0:1:(length(timeDAQ)/2-1), ...
     -length(timeDAQ)/2:1:-1] * dwData;
-
-%%
 hold on
-plot(tSample, signalsSingle);
-plot(tSample, signalsQuad);
+plot(1e3*tSample, signalsSingle/1e3, '-', 'LineWidth', 1.5);
+% plot(tSample, 2*signalsQuad);
+plot(1e3*tSample, signalsComparison/1e3, '-.', ...
+    'LineWidth', 1.5);
 hold off
+xlim([0, 300])
+set(gcf, 'position', [-1707, 248, 631, 633]);
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 12.5, ...
+    'XMinorTick', 'on', ...
+    'YMinorTick', 'on', ...
+    'TickLength', [0.0100    0.0250]);
+xlabel('$t$ [ms]', 'Interpreter', 'latex')
+ylabel('$\Delta p$ [kPa]', 'Interpreter', 'latex')
 
+legend({'Hydrophone', 'Coupled model', 'Uncoupled model'}, ...
+    'Location', 'best', 'Interpreter' ,'latex')
+
+%% Figure 1B: Shape of instant open vs. shuttle model
+figure(202); clf
+plot(1e3*timeDAQ, signalDataPlot/max(signalDataPlot), '.-k', ...
+    'MarkerSize', 2);
+hold on
+plot(1e3*tSample, signalsSingle/max(signalsSingle), '-', 'LineWidth', 1.5);
+plot(1e3*tSample, signalsComparison/max(signalsComparison), '-.', ...
+    'LineWidth', 1.5);
+hold off
+xlim([0, 100])
+ylim([-0.6, 1.2])
+set(gcf, 'position', [-1092, 448, 802, 433]);
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 14, ...
+    'XMinorTick', 'on', ...
+    'YMinorTick', 'on')
+xlabel('$t$ [ms]', 'Interpreter', 'latex')
+ylabel('$\Delta p / \max \Delta p$', 'Interpreter', 'latex')
+
+legend({'Hydrophone', 'Coupled model', 'Uncoupled model'}, ...
+    'Location', 'southwest', 'Interpreter' ,'latex')
+
+% Plot inset
+axes('Position',[.55 .55 .3 .3])
+box on
+plot(1e3*timeDAQ, signalDataPlot/max(signalDataPlot), '.-k', ...
+    'MarkerSize', 2);
+hold on
+plot(1e3*tSample, signalsSingle/max(signalsSingle), '-', 'LineWidth', 1.5);
+plot(1e3*tSample, signalsComparison/max(signalsComparison), '-.', ...
+    'LineWidth', 1.5);
+hold off
+xlim([2,16])
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 12.5, ...
+    'XMinorTick', 'on', ...
+    'YMinorTick', 'on', ...
+    'TickLength', 2*[0.0100    0.0250]);
+
+%% Figure 2: Bubble models switch out
+figure(203); clf
+% plot(1e3*timeDAQ, signalDataPlot/1e3, '.-k', ...
+%     'MarkerSize', 2);
+subplot(2,1,1);
+fSSbS = [fullStateSingle.bubbleStates];
+fSQbS = [fullStateQuad.bubbleStates];
+
+bubblet = futuresBubbleModel(1).OutputArguments{1}.bubbleContinuationTime;
+bubbleR = futuresBubbleModel(1).OutputArguments{1}.bubbleContinuationState(1,:);
+plot(1e3*bubblet, 4/3*pi*bubbleR.^3, '-', 'LineWidth', 1.5, ...
+    'Color', [0.85, 0.33, 0.1]);
+hold on
+bubblet = futuresBubbleModel(2).OutputArguments{1}.bubbleContinuationTime;
+bubbleR = futuresBubbleModel(2).OutputArguments{1}.bubbleContinuationState(1,:);
+plot(1e3*bubblet, 4/3*pi*bubbleR.^3, '--', 'LineWidth', 1.5, ...
+    'Color', [0, 0.45, 0.74]);
+hold off
+xlim([0, 300])
+set(gcf, 'position', [-1092, 448, 802, 433]);
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 14, ...
+    'XMinorTick', 'on', ...
+    'YMinorTick', 'on')
+xlabel('$t$ [ms]', 'Interpreter', 'latex')
+ylabel('$V$ [$\mathrm{m}^3$]', 'Interpreter', 'latex')
+
+legend({'Single bubble', 'Four bubbles'}, ...
+    'Location', 'northwest', 'Interpreter' ,'latex')
+
+subplot(2,1,2);
+plot(1e3*tSample, signalsSingle/1e3, '-', 'LineWidth', 1.5, ...
+    'Color', [0.85, 0.33, 0.1]);
+hold on
+plot(1e3*tSample, 2*signalsQuad/1e3, '--b', ...
+    'LineWidth', 1.5, ...
+    'Color', [0.00, 0.45, 0.74]);
+hold off
+xlim([0, 300])
+set(gcf, 'position', [-1092, 448, 670, 586]);
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 14, ...
+    'XMinorTick', 'on', ...
+    'YMinorTick', 'on')
+xlabel('$t$ [ms]', 'Interpreter', 'latex')
+ylabel('$\Delta p$ [kPa]', 'Interpreter', 'latex')
 
 %% Frequency domain analysis
-dw = 1/tSample(end);
-omegaVec = [0:1:(length(tSample)/2-1), ...
-    -length(tSample)/2:1:-1] * dw;
+idxSampling = find(tSample >= timeDAQ(end), 1, 'first');
+idxSampling = 2*ceil(idxSampling/2);
+
+tSampleFFT = tSample(1:idxSampling);
+dw = 1/tSampleFFT(end);
+omegaVec = [0:1:(length(tSampleFFT)/2-1), ...
+    -length(tSampleFFT)/2:1:-1] * dw;
 
 figure(110); clf
-loglog(omegaVecData, abs(fft(signalData)))
+loglog(omegaVecData, abs(fft(signalData))/length(omegaVecData), '.-k', ...
+    'MarkerSize', 2)
 hold on
-loglog(omegaVec, abs(fft(signalsSingle)))
-loglog(omegaVec, abs(fft(2*signalsQuad)))
+loglog(omegaVec, abs(fft(signalsSingle(1:idxSampling)))/length(omegaVec))
+loglog(omegaVec, abs(fft(2*signalsQuad(1:idxSampling)))/length(omegaVec))
 hold off
 legend(["Data", "Single bubble", "Quad"])
 
 figure(111); clf
 for i = 1:length(portAreaRatioVector)
-    loglog(omegaVec, abs(fft(signalsPortAreaRatio{i})))
+    loglog(omegaVec, abs(fft(signalsPortAreaRatio{i}(1:idxSampling))))
     hold on
 end
 legend("portAreaRatio = " + portAreaRatioVector(1:end) + "")
 
 figure(112); clf
 for i = 1:length(pressureVector)
-    loglog(omegaVec, abs(fft(signalsPressures{i})))
+    loglog(omegaVec, abs(fft(signalsPressures{i}(1:idxSampling))))
     hold on
 end
 legend("p_{init} = " + pressureVector(1:end) + " psi")
